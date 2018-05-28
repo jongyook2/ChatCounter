47f2be6ca4d97b6cefd4f454e6e5b39c05f1d708
Yoon Jong Wook <emrrn92@naver.com>
2018-05-28 20:42:28 +0900

    Format Main class XXX (not used)

---
+++ src/main/java/edu/handong/csee/java/chatcounter/WrongPathException.java
@@ -0,0 +1,23 @@
+package edu.handong.csee.java.chatcounter;
+
+/**
+ * WrongPathException Class inherits IllegalArgumnetExcetpion
+ * If input of output path is wrong, It throws Exception. 
+ * @author Farmboy
+ *
+ */
+public class WrongPathException extends IllegalArgumentException {
+	/**
+	 * Constructor 
+	 */
+	public WrongPathException() {
+		super();
+	}
+	/**
+	 * Constructor (to use error message)
+	 * @param message
+	 */
+	public WrongPathException(String message) {
+		super(message);
+	}
+}
